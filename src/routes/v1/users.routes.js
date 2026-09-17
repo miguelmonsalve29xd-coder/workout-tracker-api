a24e143ba374aad84acf46e046fd025fb2a63e55
miguelmonsalve29xd <miguelmonsalve29xd@gmail.com>
2026-09-17 15:32:10 -0500

    feat: Implementación  GET /users/:id con manejo de params y error 404

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 // Estado en memoria (simulación)
 let users = [
   {
@@ -13,10 +12,21 @@ let users = [
   }
 ];
 
-
 // GET /api/v1/users
 router.get('/', (req, res) => { 
     res.status(200).json(users);
+});
+
+// GET /users/:id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;   // 1
+  const user = users.find(u => u.id === id);   // 2
+
+  if (!user) {   // 3
+    return res.status(404).json({ error: 'Usuario no encontrado' });
+  }
+
+  res.status(200).json(user);   // 4
 });
 
 module.exports = router
